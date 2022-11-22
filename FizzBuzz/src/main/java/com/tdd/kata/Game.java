708d8948719ff470ae22db07cb94684e0807052e
raymond <yaoyu.liao@highsoft.ltd>
2022-11-22 13:46:19 +0800

    feat: add Game

---
+++ FizzBuzz/src/main/java/com/tdd/kata/Game.java
@@ -0,0 +1,25 @@
+package com.tdd.kata;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Game {
+    private final ArrayList<FBNumber> fbNumbers;
+
+    public Game(int number) {
+        fbNumbers = new ArrayList<>();
+        for (int i = 1; i <= number; i++) {
+            fbNumbers.add(new FBNumber(i));
+        }
+    }
+
+    public int size() {
+        return fbNumbers.size();
+    }
+
+    public List<String> list() {
+        return fbNumbers.stream().map(FBNumber::toString).collect(Collectors.toList());
+    }
+}
