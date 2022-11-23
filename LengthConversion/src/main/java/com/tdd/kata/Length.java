940018a744ce4c292c7064a5ae70e69f6f99fa36
raymond <yaoyu.liao@highsoft.ltd>
2022-11-23 16:45:02 +0800

    feat: update game for Inch Foot Yard

--- LengthConversion/src/main/java/com/tdd/kata/Length.java
+++ LengthConversion/src/main/java/com/tdd/kata/Length.java
@@ -1,29 +1,35 @@
 package com.tdd.kata;
 
-public class Length {
-    private final int amount;
-    private final Unit unit;
+import static java.lang.String.format;
 
-    public Length(int amount, Unit unit) {
+public class Length {
+
+    private final int amountInInch;
+    private final String unit;
+    private final int amount;
+
+    public Length(int amount, String unit) {
         this.amount = amount;
+
+        if (unit.equals("Foot")) {
+            amountInInch = 12 * amount;
+        } else if (unit.equals("Yard")) {
+            amountInInch = 3 * 12 * amount;
+        } else {
+            amountInInch = amount;
+        }
+
         this.unit = unit;
     }
 
     @Override
     public String toString() {
-        return unit.toString(amount);
+        return format("%d (%s)", amount, unit);
     }
-
 
     @Override
     public boolean equals(Object obj) {
         if (getClass() != obj.getClass()) return false;
-
-        Length another = (Length) obj;
-        return getAmountInInch() == another.getAmountInInch();
-    }
-
-    private int getAmountInInch() {
-        return unit.getAmountInInch(amount);
+        return amountInInch == ((Length) obj).amountInInch;
     }
 }
--- LengthConversion/src/test/java/com/tdd/kata/LengthTest.java
+++ LengthConversion/src/test/java/com/tdd/kata/LengthTest.java
@@ -6,34 +6,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LengthTest {
     @Test
-    public void should_1_inch_equals_1_inch() {
-        assertEquals(new Length(1, Unit.Inch), new Length(1, Unit.Inch));
+    public void should_1_inch_equals_to_1_inch() {
+        assertEquals(new Length(1, "Inch"), new Length(1, "Inch"));
     }
 
-    @Test
-    public void should_1_foot_equals_1_foot() {
-        assertEquals(new Length(1, Unit.Foot), new Length(1, Unit.Foot));
-    }
-
-    @Test
-    public void should_1_yard_equals_1_yard() {
-        assertEquals(new Length(1, Unit.Yard), new Length(1, Unit.Yard));
-    }
-    
     @Test
     public void should_display_internal_information_friendly() {
-        assertEquals("1 (Inch)", new Length(1, Unit.Inch).toString());
-        assertEquals("1 (Foot)", new Length(1, Unit.Foot).toString());
-        assertEquals("1 (Yard)", new Length(1, Unit.Yard).toString());
+        assertEquals("1 (Inch)", new Length(1, "Inch").toString());
+        assertEquals("1 (Foot)", new Length(1, "Foot").toString());
+
     }
 
     @Test
-    public void should_1_foot_equals_to_12_inch() {
-        assertEquals(new Length(12, Unit.Inch), new Length(1, Unit.Foot));
+    public void should_1_foot_equals_to_12_inches() {
+        assertEquals(new Length(12, "Inch"), new Length(1, "Foot"));
     }
 
     @Test
     public void should_1_yard_equals_to_3_feet() {
-        assertEquals(new Length(3, Unit.Foot), new Length(1, Unit.Yard));
+        assertEquals(new Length(3, "Foot"), new Length(1, "Yard"));
     }
+
 }
